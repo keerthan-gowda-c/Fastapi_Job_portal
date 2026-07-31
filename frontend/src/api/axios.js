65d3af5b3a933cffbac9b2ced0ae40b465f33a1e
unknown <keerthan-gowda-c>
2026-07-31 21:34:37 +0530

    message

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({baseURL:"http://http://3.110.165.250/:8000"});
+const api = axios.create({baseURL:"http://3.110.165.250/:8000"});
 
 
 api.interceptors.request.use((config)=>{
